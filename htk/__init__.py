9d1d8618e9b91df54a14e11606b43ac243bdce5a
Jonathan Tsai <hello@jontsai.com>
2020-08-29 01:23:53 +0000

    htk v1.1.0 -- adds fdebug

--- htk/__init__.py
+++ htk/__init__.py
@@ -1,3 +1,5 @@
-# HTKlite Imports
 # Local Imports
 from .utils import *
+
+
+__version__ = '1.1.0'
--- htk/utils/debuggers.py
+++ htk/utils/debuggers.py
@@ -8,6 +8,21 @@ import json
 from ..encoders import DecimalEncoder
 
 
+def fdebug(file_path, text):
+    with open(file_path, 'w') as f:
+        f.write(text)
+
+
+def fdebug_json(file_path, obj, label='JSON'):
+    fdebug(
+        file_path,
+        '%s: ```%s```' % (
+            label,
+            json.dumps(obj, indent=2, cls=DecimalEncoder),
+        )
+    )
+
+
 def slack_debug(text):
     from .slack import slack_message
 
